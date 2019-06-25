3172d6a7aafd3ce619a04bb482291721c2544310
Douglas Uretsky <douglasuretsky@gmail.com>
2019-06-25 19:21:31 -0400

    update script source

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require('express');
 const socket = require('socket.io')
 const app = express();
 
-const server = app.listen(process.env.PORT)
+const server = app.listen(process.env.PORT || 4000, function() {
+    console.log('I\'m listening...')
+})
 
 app.use(express.static('public'))
 
